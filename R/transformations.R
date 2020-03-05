44b00686956a194de24166d71720c2c4fbfc7bb8
David Holt <d@tdfd.org>
2020-03-05 14:27:54 -0500

    update transformations: - remove library(dplyr) calls - remove pipes (replaced with nesting) - use rlang::is_attached to detect furrr package

--- R/transformations.R
+++ R/transformations.R
@@ -14,18 +14,18 @@ add_decomposition <-
         }
         obs_per_season <- frequency(tsbl)
 
-        library(dplyr)
         tsbl <-
-            tsbl %>%
-            fabletools::model(feasts::STL(value, robust = TRUE)) %>%
-            fabletools::components() %>%
-            dplyr::select(-.model)
+            dplyr::select(
+                fabletools::components(
+                    fabletools::model(
+                        tsbl,
+                        feasts::STL(value, robust = TRUE))),
+                -.model)
         component_names <- names(tsbl)[which(!names(tsbl) %in% c("index", "value", tsibble::key_vars(tsbl)))]
         names(tsbl)[which(names(tsbl) %in% component_names)] <- paste0(".", component_names)
 
         if (long) {
-            pivot_tsbl_measures_longer(tsbl, key_name = "decomp_key") %>%
-                clean_tsbl()
+            clean_tsbl(pivot_tsbl_measures_longer(tsbl, key_name = "decomp_key"))
         } else {
             clean_tsbl(tsbl)
         }
@@ -50,19 +50,16 @@ remove_outliers <-
             add_clean_values <-
                 function(tsbl) {
                     xts_data <- tsbox::ts_xts(tsbl)
-                    tsbl %>%
-                        mutate(.clean = forecast::tsclean(xts_data) %>% as.double(),
-                               .outlier = ifelse(xts_data == .clean, FALSE, TRUE) %>% as.logical())
+                    dplyr::mutate(
+                        tsbl,
+                        .clean = as.double(forecast::tsclean(xts_data)),
+                        .outlier = as.logical(ifelse(xts_data == .clean, FALSE, TRUE)))
+
                 }
 
-            library(dplyr)
+            tbl_nested <- tidyr::nest(tsibble::group_by_key(tsbl))
 
-            tbl_nested <-
-                tsbl %>%
-                tsibble::group_by_key() %>%
-                tidyr::nest()
-
-            if ("furrr" %in% (.packages())) {
+            if (rlang::is_attached("package::furrr")) {
                 tbl_nested <-
                     dplyr::mutate(tbl_nested, data = furrr::future_map(data, add_clean_values))
             } else {
@@ -71,19 +68,22 @@ remove_outliers <-
             }
 
             tsbl_clean <-
-                tbl_nested %>%
-                tidyr::unnest(cols = c(data)) %>%
-                tsibble::as_tsibble(key = tsibble::key_vars(tsbl), index = tsibble::index_var(tsbl))
+                tsibble::as_tsibble(
+                    tidyr::unnest(
+                        tbl_nested,
+                        cols = c(data)),
+                    key = tsibble::key_vars(tsbl),
+                    index = tsibble::index_var(tsbl))
 
             if (any(tsbl_clean$.outlier)) {
                 message("No outliers found. Returning original tsibble.")
                 clean_tsbl(tsbl)
             } else {
                 if (long) {
-                    tsbl_clean %>%
-                        select(-.outlier) %>%
-                        pivot_tsbl_measures_longer(key_name = "outlier_key") %>%
-                        clean_tsbl()
+                    clean_tsbl(
+                        pivot_tsbl_measures_longer(
+                            select(tsbl_clean, -.outlier),
+                            key_name = "outlier_key"))
                 } else {
                     clean_tsbl(tsbl_clean)
                 }
@@ -117,130 +117,129 @@ impute_values <-
         seasonal_splitting = FALSE) {
 
         method <- match.arg(method)
-        library(dplyr)
 
         add_inputed_value <-
             function(tsbl) {
                 xts_data <- tsbox::ts_xts(tsbl)
                 if (method == "linear") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_interpolation(xts_data, option = "linear") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_interpolation(xts_data, option = "linear")),
                            .imputed_method = "linear interpolation")
                 } else if (method == "spline") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_interpolation(xts_data, option = "spline") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_interpolation(xts_data, option = "spline")),
                            .imputed_method = "spline interpolation")
                 } else if (method == "stine") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_interpolation(xts_data, option = "stine") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_interpolation(xts_data, option = "stine")),
                            .imputed_method = "stine interpolation")
                 } else if (method == "ma") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_ma(xts_data, k = value) %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_ma(xts_data, k = value)),
                            .imputed_method = "ma")
                 } else if (method == "structTS") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_kalman(xts_data, model = "StructTS", smooth = Kalman_smooth) %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_kalman(xts_data, model = "StructTS", smooth = Kalman_smooth)),
                            .imputed_method = "StructTS")
                 } else if (method == "ARIMA") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_kalman(xts_data, model = "ARIMA", smooth = Kalman_smooth) %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_kalman(xts_data, model = "ARIMA", smooth = Kalman_smooth)),
                            .imputed_method = "ARIMA")
                 } else if (method == "locf") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_locf(xts_data, na_remaining = "rev") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_locf(xts_data, na_remaining = "rev")),
                            .imputed_method = "locf")
                 } else if (method == "nocb") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_locf(xts_data, option = "nocb", na_remaining = "rev") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_locf(xts_data, option = "nocb", na_remaining = "rev")),
                            .imputed_method = "nocb")
                 } else if (method == "mean") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_mean(xts_data) %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_mean(xts_data)),
                            .imputed_method = "mean")
                 } else if (method == "median") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_mean(xts_data, option = "median") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_mean(xts_data, option = "median")),
                            .imputed_method = "median")
                 } else if (method == "mode") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_mean(xts_data, option = "mode") %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_mean(xts_data, option = "mode")),
                            .imputed_method = "mode")
                 } else if (method == "random") {
-                    mutate(tsbl,
-                           .imputed_value = imputeTS::na_random(xts_data) %>% as.double(),
+                    dplyr::mutate(tsbl,
+                           .imputed_value = as.double(imputeTS::na_random(xts_data)),
                            .imputed_method = "random")
                 } else if (method == "seasonal_interpolation") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "interpolation") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "interpolation")),
                                .imputed_method = "seadec_interpolation")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "interpolation") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "interpolation")),
                                .imputed_method = "seasplit_interpolation")
                     }
                 } else if (method == "seasonal_locf") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "locf") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "locf")),
                                .imputed_method = "seasonal_decomp_locf")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "locf") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "locf")),
                                .imputed_method = "seasonal_split_locf")
                     }
                 } else if (method == "seasonal_mean") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "mean") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "mean")),
                                .imputed_method = "seasonal_decomp_mean")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "mean") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "mean")),
                                .imputed_method = "seasonal_split_mean")
                     }
                 } else if (method == "seasonal_random") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "random") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "random")),
                                .imputed_method = "seasonal_decomp_random")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "random") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "random")),
                                .imputed_method = "seasonal_split_random")
                     }
                 } else if (method == "seasonal_kalman") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "kalman") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "kalman")),
                                .imputed_method = "seasonal_decomp_kalman")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "kalman") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "kalman")),
                                .imputed_method = "seasonal_split_kalman")
                     }
                 } else if (method == "seasonal_ma") {
                     if (seasonal_splitting) {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seadec(xts_data, algorithm = "ma") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seadec(xts_data, algorithm = "ma")),
                                .imputed_method = "seasonal_decomp_ma")
                     } else {
-                        mutate(tsbl,
-                               .imputed_value = imputeTS::na_seasplit(xts_data, algorithm = "ma") %>% as.double(),
+                        dplyr::mutate(tsbl,
+                               .imputed_value = as.double(imputeTS::na_seasplit(xts_data, algorithm = "ma")),
                                .imputed_method = "seasonal_split_ma")
                     }
                 } else if (method == "replace") {
                     dplyr::mutate(tsbl,
-                           .imputed_value = imputeTS::na_replace(xts_data, value) %>% as.double(),
+                           .imputed_value = as.double(imputeTS::na_replace(xts_data, value)),
                            .imputed_method = "replace")
                 }
             }
 
         tbl_nested <-
-            tsbl %>%
-            tsibble::group_by_key() %>%
-            tidyr::nest(data = everything())
+            tidyr::nest(
+                tsibble::group_by_key(tsbl),
+                data = everything())
 
         if ("furrr" %in% (.packages())) {
             tbl_nested <-
@@ -251,19 +250,24 @@ impute_values <-
         }
 
         tsbl_imputed <-
-            tbl_nested %>%
-            tidyr::unnest(cols = c(data)) %>%
-            tsibble::as_tsibble(key = tsibble::key_vars(tsbl), index = tsibble::index_var(tsbl))
+            tsibble::as_tsibble(
+                tidyr::unnest(
+                    tbl_nested,
+                    cols = c(data)),
+                key = tsibble::key_vars(tsbl),
+                index = tsibble::index_var(tsbl))
 
         if (any(tsbl_imputed$value != tsbl_imputed$.imputed_value)) {
             message("No values imputed. Returning original tsibble.")
             clean_tsbl(tsbl)
         } else {
             if (long) {
-                tsbl_imputed %>%
-                    select(-.imputed_method) %>%
-                    pivot_tsbl_measures_longer(key_name = "imputed_key") %>%
-                    clean_tsbl()
+                clean_tsbl(
+                    pivot_tsbl_measures_longer(
+                        select(
+                            tsbl_imputed,
+                            -.imputed_method),
+                        key_name = "imputed_key"))
             } else {
                 clean_tsbl(tsbl_imputed)
             }
